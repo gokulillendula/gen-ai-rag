19c1c4f5b1e905cb5a1b99f07a8c5cbd7b09f27c
Illendula Gokul <gokulillendula@gmail.com>
2026-02-26 10:30:08 +0530

    second commit

--- rag_project/settings.py
+++ rag_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c)ir9n1onmznli!+oizta&s)l!_9l6ff!q1&1)q@=hsxr9n2yu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
